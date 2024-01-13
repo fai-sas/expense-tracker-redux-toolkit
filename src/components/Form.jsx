c58acc7c3a220438bcb0fea9c1ca46f91eb6420e
fai-sas <craftedbyfaisal@gmail.com>
2024-01-13 20:26:22 +0600

    add transaction functionality added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,51 +1,98 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-expressions */
+
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { createTransaction } from "../features/transaction/transactionSlice"
+
 export default function Form() {
-    return (
-        <div className="form">
-            <h3>Add new transaction</h3>
+  const [name, setName] = useState("")
+  const [type, setType] = useState("")
+  const [amount, setAmount] = useState("")
+  const dispatch = useDispatch()
 
-            <div className="form-group">
-                <label for="transaction_name">Name</label>
-                <input
-                    type="text"
-                    name="transaction_name"
-                    placeholder="My Salary"
-                />
-            </div>
+  const { isLoading, isError } = useSelector((state) => state.transaction)
 
-            <div className="form-group radio">
-                <label for="transaction_type">Type</label>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="income"
-                        name="transaction_type"
-                        checked
-                    />
-                    <label for="transaction_type">Income</label>
-                </div>
-                <div className="radio_group">
-                    <input
-                        type="radio"
-                        value="expense"
-                        name="transaction_type"
-                        placeholder="Expense"
-                    />
-                    <label for="transaction_type">Expense</label>
-                </div>
-            </div>
+  const handleCreate = (e) => {
+    e.preventDefault()
 
-            <div className="form-group">
-                <label for="transaction_amount">Amount</label>
-                <input
-                    type="number"
-                    placeholder="300"
-                    name="transaction_amount"
-                />
-            </div>
+    const data = { name, type, amount: Number(amount) }
 
-            <button className="btn">Add Transaction</button>
+    dispatch(createTransaction(data))
 
-            <button className="btn cancel_edit">Cancel Edit</button>
+    setName("")
+    setType("")
+    setAmount("")
+  }
+
+  return (
+    <div className="form">
+      <h3>Add new transaction</h3>
+
+      <form onSubmit={handleCreate}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
         </div>
-    );
+
+        <div className="form-group radio">
+          <label>Type</label>
+          <div className="radio_group">
+            <input
+              type="radio"
+              value={type}
+              name="type"
+              checked={type === "income"}
+              onChange={(e) => setType("income")}
+              required
+            />
+            <label>Income</label>
+          </div>
+          <div className="radio_group">
+            <input
+              type="radio"
+              value="expense"
+              name="type"
+              placeholder="Expense"
+              checked={type === "expense"}
+              onChange={(e) => setType("expense")}
+              required
+            />
+            <label>Expense</label>
+          </div>
+        </div>
+
+        <div className="form-group">
+          <label>Amount</label>
+          <input
+            type="number"
+            placeholder="enter amount"
+            name="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn" disabled={isLoading}>
+          Add Transaction
+        </button>
+
+        {!isLoading && isError && (
+          <p className="error">There was an error occurred</p>
+        )}
+      </form>
+
+      <button className="btn cancel_edit">Cancel Edit</button>
+    </div>
+  )
 }
